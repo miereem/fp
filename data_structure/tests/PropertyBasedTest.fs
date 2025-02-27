fac99c4289e6878994374497a404d32d4eb72743
miereem <mabdurasulkyzy@gmail.com>
2025-02-28 02:04:23 +0300

    unit tests added

---
+++ data_structure/tests/PropertyBasedTest.fs
@@ -0,0 +1,122 @@
+﻿module MultiSetPropertyTests
+
+open NUnit.Framework
+open FsCheck
+open Multiset
+open System
+open System.IO
+
+// Helper function to compare two MultiSets for equality
+let areMultiSetsEqual (set1: MultiSet<'T>) (set2: MultiSet<'T>) =
+    if set1.Size <> set2.Size then
+        false
+    else
+        set1.Table
+        |> Array.forall2 (fun x y ->
+            match x, y with
+            | Some (_, e1, c1), Some (_, e2, c2) -> e1 = e2 && c1 = c2
+            | None, None -> true
+            | _ -> false) set2.Table
+
+// Generator for creating random MultiSets
+let generateMultiSet =
+    Gen.sized (fun size ->
+        gen {
+            let! elements = Gen.listOfLength size (Arb.generate<int>)
+            let set = empty (size * 2)
+
+            let filledSet =
+                List.fold (fun acc e -> add e 1 acc) set elements
+
+            return filledSet
+        })
+
+let testMultiSet = Arb.fromGen generateMultiSet
+
+// Property test: Adding an element increases its count
+[<Test>]
+let ``Add Property - Adding Element Increases Count``() =
+    let property (set: MultiSet<int>) =
+        let element = 54
+        let newSet = add element 1 set
+        count element newSet = count element set + 1
+
+    Prop.forAll testMultiSet property
+    |> Check.QuickThrowOnFailure
+
+// Property test: Adding zero or negative count does not change the set
+[<Test>]
+let ``Add Property - Adding Zero or Negative Count Does Not Change Set``() =
+    let property (set: MultiSet<int>) =
+        let element = 56
+        let newSet = add element 0 set
+        areMultiSetsEqual set newSet
+
+    Prop.forAll testMultiSet property
+    |> Check.QuickThrowOnFailure
+
+// Property test: Count of an element not in the set is zero
+[<Test>]
+let ``Count Property - Count of Missing Element is Zero``() =
+    let property (set: MultiSet<int>) =
+        let element = 89
+        not (set.Table |> Array.exists (function
+            | Some (_, e, _) -> e = element
+            | None -> false))
+        ==> lazy (count element set = 0)
+
+    Prop.forAll testMultiSet property
+    |> Check.QuickThrowOnFailure
+
+// Property test: Union with an empty set returns the original set
+[<Test>]
+let ``Union Property - Union with Empty Set Returns Original Set``() =
+    let property (set: MultiSet<int>) =
+        let emptySet = empty 16
+        let unionSet = union set emptySet
+        areMultiSetsEqual set unionSet
+
+    Prop.forAll testMultiSet property
+    |> Check.QuickThrowOnFailure
+
+// Property test: Union is associative
+[<Test>]
+let ``Associative Property - Union is Associative``() =
+    let property
+        (
+            set1: MultiSet<int>,
+            set2: MultiSet<int>,
+            set3: MultiSet<int>
+        ) =
+        let unionLeft = union (union set1 set2) set3
+        let unionRight = union set1 (union set2 set3)
+        areMultiSetsEqual unionLeft unionRight
+
+    let testTripleSet = Arb.fromGen (Gen.zip3 generateMultiSet generateMultiSet generateMultiSet)
+
+    Prop.forAll testTripleSet property
+    |> Check.QuickThrowOnFailure
+
+// Property test: Empty set is empty
+[<Test>]
+let ``IsEmpty Property - Empty Set is Empty``() =
+    let property () =
+        let emptySet = empty 16
+        isEmpty emptySet
+
+    Check.QuickThrowOnFailure property
+
+// Property test: Non-empty set is not empty
+[<Test>]
+let ``IsEmpty Property - Non-Empty Set is Not Empty``() =
+    let property (element: int) =
+        let set = add element 1 (empty 16)
+        not (isEmpty set)
+
+    Check.QuickThrowOnFailure property
+
+// SetUp method to prepare the environment before running tests
+[<OneTimeSetUp>]
+let initialize() =
+    let stringWriter = new StringWriter()
+    System.Console.SetOut(stringWriter)
---
+++ data_structure/tests/UnitTest.fs
@@ -0,0 +1,53 @@
+module MultiSetTests
+
+open NUnit.Framework
+open FsCheck
+open FsCheck.NUnit
+open Multiset
+
+[<Test>]
+let ``Empty MultiSet should be empty`` () =
+    let ms = empty 10
+    Assert.IsTrue(isEmpty ms)
+
+[<Test>]
+let ``Adding an element should increase its count`` () =
+    let ms = empty 10 |> add "apple" 2
+    Assert.AreEqual(2, count "apple" ms)
+
+[<Test>]
+let ``Adding zero or negative count should not change the MultiSet`` () =
+    let ms = empty 10 |> add "banana" 0
+    Assert.AreEqual(0, count "banana" ms)
+    let ms2 = ms |> add "banana" -3
+    Assert.AreEqual(0, count "banana" ms2)
+
+[<Test>]
+let ``Adding the same element multiple times should accumulate counts`` () =
+    let ms = empty 10 |> add "cherry" 2 |> add "cherry" 3
+    Assert.AreEqual(5, count "cherry" ms)
+
+[<Test>]
+let ``Union of two MultiSets should merge counts`` () =
+    let ms1 = empty 10 |> add "dog" 3
+    let ms2 = empty 10 |> add "dog" 2 |> add "cat" 1
+    let result = union ms1 ms2
+    Assert.AreEqual(5, count "dog" result)
+    Assert.AreEqual(1, count "cat" result)
+
+[<Test>]
+let ``Union with empty MultiSet should not change`` () =
+    let ms = empty 10 |> add "elephant" 4
+    let result = union ms (empty 10)
+    Assert.AreEqual(4, count "elephant" result)
+
+[<Property>]
+let ``Adding an element increases size if it was not present before`` (x: string) =
+    let ms = empty 10
+    let newMs = add x 1 ms
+    isEmpty ms ==> (newMs.Size = 1)
+
+[<Property>]
+let ``Count should never be negative`` (x: string) (n: int) =
+    let ms = empty 10 |> add x n
+    count x ms >= 0
